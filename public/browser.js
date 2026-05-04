21a6b213b7a5ceb0b71505c9827a57aa92b360ba
Max010702 <hamidzonovmuhriddin@gmail.com>
2026-05-04 14:52:27 +0900

    feat: reja loyihamizning yakuniy bosqichi logic

--- public/browser.js
+++ public/browser.js
@@ -1,3 +1,5 @@
+// const { response } = require("../app");
+
 console.log("FrontEnd JS isjga tushdi");
 
 function itemTemplate(item) {
@@ -51,6 +53,33 @@ document.addEventListener("click", function (e) {
 
   // edit oper
   if (e.target.classList.contains("edit-me")) {
-    alert("siz edit tugmasini bosdingiz");
+    let userInput = prompt(
+      "Ozgartirish kiriting",
+      e.target.parentElement.parentElement.querySelector(".item-text")
+        .innerHTML,
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(
+            ".item-text",
+          ).innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan xarakat qiling");
+        });
+    }
   }
+});
+
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", { delete_all: true }).then((response) => {
+    alert(response.data.state);
+    document.location.reload();
+  });
 });
--- train.js
+++ train.js
@@ -1,11 +1,65 @@
-// MIT TASK C
+// MIT TASK D
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
 
-function checkContent(str1, str2) {
-  return str1.split("").sort().join("") === str2.split("").sort().join("");
+  #getTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  }
+
+  qoldiq() {
+    console.log(
+      `Hozir ${this.#getTime()}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`,
+    );
+  }
+
+  sotish(mahsulot, miqdor) {
+    if (this[mahsulot] === undefined) {
+      console.log(`${mahsulot} mavjud emas!`);
+      return;
+    }
+    if (this[mahsulot] < miqdor) {
+      console.log(`Yetarli ${mahsulot} yo'q! Faqat ${this[mahsulot]}ta bor.`);
+      return;
+    }
+    this[mahsulot] -= miqdor;
+    console.log(`${this.#getTime()}da ${miqdor}ta ${mahsulot} sotildi.`);
+  }
+
+  qabul(mahsulot, miqdor) {
+    if (this[mahsulot] === undefined) {
+      console.log(`${mahsulot} mavjud emas!`);
+      return;
+    }
+    this[mahsulot] += miqdor;
+    console.log(`${this.#getTime()}da ${miqdor}ta ${mahsulot} qabul qilindi.`);
+  }
 }
 
-console.log(checkContent("mitgroup", "gmtiprou"));
-console.log(checkContent("uzbekistan", "southkorea"));
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();
+
+shop.sotish("non", 3);
+
+shop.qabul("cola", 4);
+
+shop.qoldiq();
+
+// // MIT TASK C
+
+// function checkContent(str1, str2) {
+//   return str1.split("").sort().join("") === str2.split("").sort().join("");
+// }
+
+// console.log(checkContent("mitgroup", "gmtiprou"));
+// console.log(checkContent("uzbekistan", "southkorea"));
 
 // // MIT TASK B
 // function countDigits(str) {
